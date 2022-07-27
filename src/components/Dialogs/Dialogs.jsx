8966212f635c0d6f5266d46e5804e19d5abcd1d5
Vladisalv <warboyka48@gmail.com>
2022-07-28 00:30:43 +0300

    added new page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-import style from './Dialogs.module.css'
+import styles from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={`${styles.dialog} ${styles.active}`}>Dima</div>
+        <div className={styles.dialog}>Alex</div>
+        <div className={styles.dialog}>Vadim</div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hello!</div>
+        <div className={styles.message}>How are you?</div>
+        <div className={styles.message}>Lol</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
+
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <div className={styles.item}>
-        <a href="/profile">Profile</a>
+        <NavLink
+          to="/profile"
+          className={(navData) =>
+            navData.isActive ? styles.active : styles.item
+          }
+        >
+          Profile
+        </NavLink>
       </div>
-      <div className={`${styles.item} ${styles.active}`}>
-        <a href="/dialogs">Messages</a>
+      <div className={styles.item}>
+        <NavLink
+          to="/dialogs"
+          className={(navData) =>
+            navData.isActive ? styles.active : styles.item
+          }
+        >
+          Messages
+        </NavLink>
       </div>
       <div className={styles.item}>
         <a>News</a>
