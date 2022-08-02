58718f019ad30e997f9fc13d3788dddcf42dec81
Vladisalv <warboyka48@gmail.com>
2022-08-02 21:35:29 +0300

    fixed

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,47 +1,47 @@
-import { root } from "../index";
+import { root } from '../index';
 
 export const store = {
-    _state: {
-        profilePage: {
-            postsData: [
-                { id: 1, message: "Hello, world!", likesCount: 12 },
-                { id: 2, message: "It is my second post", likesCount: 11 },
-            ],
-            newPostText: '',
-        },
-        dialogsPage: {
-            messagesData: [
-                { id: 1, message: "Hello" },
-                { id: 2, message: "How are you?" },
-                { id: 3, message: "Good!" },
-            ],
-            dialogsData: [
-                { id: 1, name: "Dima" },
-                { id: 2, name: "Alex" },
-                { id: 3, name: "Dasha" },
-            ],
-        }
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: 'Hello, world!', likesCount: 12 },
+        { id: 2, message: 'It is my second post', likesCount: 11 },
+      ],
+      newPostText: '',
     },
-    getState() {
-        return this._state;
+    dialogsPage: {
+      messagesData: [
+        { id: 1, message: 'Hello' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'Good!' },
+      ],
+      dialogsData: [
+        { id: 1, name: 'Dima' },
+        { id: 2, name: 'Alex' },
+        { id: 3, name: 'Dasha' },
+      ],
     },
-    _subscriber() { },
-    addPost() {
-        const newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0,
-        }
-        if (this._state.profilePage.newPostText === '') return;
-        this._state.profilePage.postsData.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._subscriber(root, store);
-    },
-    updateNewPostChange(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._subscriber(root, store);
-    },
-    subscriber(observer) {
-        this._subscriber = observer;
-    }
-}
+  },
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {},
+  addPost() {
+    const newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    if (this._state.profilePage.newPostText === '') return;
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(root, store);
+  },
+  updateNewPostChange(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(root, store);
+  },
+  subscriber(observer) {
+    this._callSubscriber = observer;
+  },
+};
