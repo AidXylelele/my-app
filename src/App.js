eebb606f49f3d0c4b3d50cbd5641608b14dbb542
Vladisalv <warboyka48@gmail.com>
2022-08-07 01:35:11 +0300

    hot fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 
