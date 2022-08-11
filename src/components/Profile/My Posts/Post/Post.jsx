e2350bf6c0a0da6e13acd20ec8fa5d1f064d7e06
AIDXYLELELE <warboyka48@gmail.com>
2022-08-11 19:12:46 +0300

    added some styles

--- src/components/Profile/My Posts/Post/Post.jsx
+++ src/components/Profile/My Posts/Post/Post.jsx
@@ -5,13 +5,22 @@ const Post = (props) => {
   return (
     <div className={styles.item}>
       <img
+        className={styles.avatarOfUser}
         src="https://cdn-icons-png.flaticon.com/128/6676/6676016.png"
         alt="There is an avatar"
       ></img>
-      {props.message}
-      <div>like: {props.like}</div>
+      <div className={styles.textOfPost}>
+        <p>{props.message}</p>
+      </div>
+      <div className={styles.reactionsOfUsers}>
+        <div className={styles.replyToPost}>
+          <a>Reply</a>
+        </div>
+        <div className={styles.counterOfLikes}>
+          <a>like: {props.like}</a>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Post;
