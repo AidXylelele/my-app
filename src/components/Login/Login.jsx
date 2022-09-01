94a2e79a81765fe7e9d4fcab4afbfd905e7e6e7d
Vladisalv <warboyka48@gmail.com>
2022-09-01 15:12:08 +0300

    refactored and used react-hook-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const LoginForm = (props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  // console.log(watch('email'));
+  // console.log(watch('name'));
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <input
+          placeholder="Name"
+          {...register('name', { required: 'This field is required' })}
+        />
+        {errors.name && <div>{errors.name.message}</div>}
+      </div>
+      <div>
+        <input
+          placeholder="E-mail"
+          {...register('email', {
+            required: 'This field is required',
+            pattern: {
+              value:
+                /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+              message: 'Please enter a valid E-mail!',
+            },
+          })}
+        />
+        {errors.email && <div>{errors.email.message}</div>}
+      </div>
+
+      <input type="submit" />
+    </form>
+  );
+};
 
 const Login = (props) => {
   return (
-    <div>
-      <h1>LoginPage</h1>
-    </div>
+    <>
+      <h1>Login</h1>
+      <LoginForm />
+    </>
   );
 };
 
