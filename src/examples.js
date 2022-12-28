0b0e6226a7e762e580a3d3a1f0d20824d943a6f8
Vladislav Shevyrov <89361346+AidXylelele@users.noreply.github.com>
2022-12-28 18:27:09 +0200

    refactored

--- src/examples.js
+++ src/examples.js
@@ -1,13 +1,10 @@
-function getPostDate () {
-    const date = new Date(Date.now()).toDateString();
-
-    const data = date.split(' ');
-
-    const postDate = {
-      dayOfWeek: data[0],
-      day: data[2],
-      month: data[1],
-      year: data[3],
-    };
-    return postDate;
+function getPostDate() {
+  const date = new Date(Date.now()).toDateString();
+  const [dayOfWeek, month, day, year] = date.split(' ');
+  return {
+    dayOfWeek,
+    day,
+    month,
+    year,
+  };
 }
