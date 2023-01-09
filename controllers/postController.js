4360f4bc446354eae6abddf53513220f1097c771
LikerFeed <vanya18845@gmail.com>
2023-01-09 21:56:15 +0200

    Imported collection without destructuring

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,70 +5,68 @@ const {
   deletePost,
 } = require('../models/posts');
 
-async function getPostsController(params) {
-  return await getPosts(params.id).then((posts) => {
-    if (posts) {
+class PostControllers {
+  static getPostsController = async (params) => {
+    return await getPosts(params.id).then((posts) => {
+      if (posts) {
+        return {
+          posts,
+          resultCode: 0,
+        };
+      }
       return {
-        posts,
-        resultCode: 0,
+        messages: 'User with the same E-mail was created!',
+        resultCode: 1,
       };
-    }
-    return {
-      messages: 'User with the same E-mail was created!',
-      resultCode: 1,
-    };
-  });
-}
+    });
+  };
 
-async function createPostController(message, { id }) {
-  const date = Date.now();
-  return await createPost(message, id, date).then((post) => {
-    if (post) {
-      return {
-        post,
-        resultCode: 0,
-      };
-    }
-    return { messages: 'Something went wrong!', resultCode: 1 };
-  });
-}
+  static createPostController = async (message, { id }) => {
+    const date = Date.now();
+    return await createPost(message, id, date).then((post) => {
+      if (post) {
+        return {
+          post,
+          resultCode: 0,
+        };
+      }
+      return { messages: 'Something went wrong!', resultCode: 1 };
+    });
+  }
 
-async function updatePostController(message, id) {
-  const date = Date.now();
-  return await updatePost(message, id, date).then((result) => {
-    if (result) {
-      return {
-        messages: 'Success!',
-        resultCode: 0,
-      };
-    }
-    return { messages: 'Something went wrong!', resultCode: 1 };
-  });
-}
+  static updatePostController = async (message, id) => {
+    const date = Date.now();
+    return await updatePost(message, id, date).then((result) => {
+      if (result) {
+        return {
+          messages: 'Success!',
+          resultCode: 0,
+        };
+      }
+      return { messages: 'Something went wrong!', resultCode: 1 };
+    });
+  }
 
-const deletePostController = async ({ id }) => {
-  return await deletePost(id).then((result) => {
-    if (result) {
-      return {
-        messages: 'Successfully deleted!',
-        post: {
+  static deletePostController = async ({ id }) => {
+    return await deletePost(id).then((result) => {
+      if (result) {
+        return {
+          messages: 'Successfully deleted!',
+          post: {
           post_id: id,
         },
         resultCode: 0,
-      };
-    }
-    return { messages: 'Something went wrong!', resultCode: 1 };
-  });
-};
-
+        };
+      }
+      return { messages: 'Something went wrong!', resultCode: 1 };
+    });
+  };
+}
 
 
 
 
 
 module.exports = {
-  getPostsController,
-  createPostController,
-  updatePostController,
-  deletePostController,
+  PostControllers
 };
