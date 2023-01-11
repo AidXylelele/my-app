6b42005855b1b8883982d17f655cb4d61e0ff24f
olegoofy <martynenkooleg77@gmail.com>
2023-01-11 19:44:36 +0200

    add get likes controller

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -1,3 +1,20 @@
-class likesController {
-  static;
+const LikeModel = require('../models/likes');
+
+class LikeController {
+  static getLikesController = async (params) => {
+    return await LikeModel.getLikes(params.id).then((likes) => {
+      if (likes) {
+        return {
+          likes,
+          resultCode: 0,
+        };
+      }
+      return {
+        messages: 'Oops! Something went wrong',
+        resultCode: 1,
+      };
+    });
+  };
 }
+
+module.exports = LikeController;
