39abd2088fb3ded55842211f6ee8cb84595ec99e
LikerFeed <vanya18845@gmail.com>
2023-01-13 00:00:43 +0200

    Fixed

--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -11,7 +11,7 @@ export const SidebarData = [
     cName: 'nav_text',
   },
   {
-    title: 'Messager',
+    title: 'Messenger',
     path: '/dialogs',
     icon: <VscIcons.VscCommentDiscussion style={{ color: 'black' }} />,
     cName: 'nav_text',
